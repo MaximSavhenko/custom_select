a80ddc8cd8001fa655345ea3058b28729054c96f
Maxim Savchenko <maxim.savchenko@wdg.com.ua>
2021-03-04 09:11:47 +0200

    fix sel

--- js/main.js
+++ js/main.js
@@ -2,33 +2,55 @@ function customSelect() {
     let btnSelect = document.querySelectorAll('.select__btn'),
         listSelect = document.querySelectorAll('.select__list'),
         itemsSelect = document.querySelectorAll('.select__item'),
-        inputSelect = document.querySelectorAll('.select__input')
+        inputSelect = document.querySelectorAll('.select__input'),
+        select = document.querySelectorAll('.select')
 
         for (let i = 0; i < btnSelect.length; i++) {
-            btnSelect[i].onclick = function () {
+            btnSelect[i].addEventListener("click", (function(i){return function(event){
                 listSelect[i].classList.toggle('active');
-            }; 
+            }})(i));
         }
+
+        
         
        
-
+        
 
         // itemsSelect.forEach(items => {
         //     items.onclick = function (e) {
-        //             btn.innerText = e.target.innerText;
-        //             // input.value = e.target.dataset.value;
-        //             // list.classList.remove('active');    
+        //         btnSelect.innerText = e.target.innerText;
+        //         inputSelect.value = e.target.dataset.value;
+        //         listSelect.classList.remove('active');    
         //     };
         // });
 
-       
-        for (let i = 0; i < listSelect.length; i++) {
+        
+            for (let i = 0; i < itemsSelect.length; i++) {
+                itemsSelect[i].onclick = function (event) {
+                    btnSelect[i].innerText = event.target.innerText;
+                    inputSelect[i].value = event.target.dataset.value;
+                    listSelect[i].classList.remove('active');  
+                };
+            }
+            
 
-            listSelect[i].addEventListener("click", (function(i){return function(event){
-                btnSelect[i].innerText = event.target.innerText;
-                inputSelect[i].value = event.target.dataset.value;
-            }})(i));
-        }
+       
+        
+
+        // for (let j = 0; j < select.length; j++) {
+        //     for (let i = 0; i < itemsSelect.length; i++) {
+
+        //         itemsSelect[j].onclick = function (e) {
+        //             btnSelect[j].innerText = event.target.innerText;
+        //             inputSelect[j].value = event.target.dataset.value;
+                    // listSelect[j].classList.remove('active');  
+        //         };
+        //     }
+        // }
+
+        
+
+        
         
 
 
